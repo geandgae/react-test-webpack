ac32c1706b8d05fb3e00628405be27b04e57ff72
ge <g_e@daum.net>
2024-06-13 21:41:27 +0900

    mui set

---
+++ src/components/HeaderMui.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+const HeaderMui = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton edge="start" color="inherit" aria-label="menu">
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" style={{ flexGrow: 1 }}>
+          My React App
+        </Typography>
+        <Button color="inherit">Home</Button>
+        <Button color="inherit">About</Button>
+        <Button color="inherit">Contact</Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default HeaderMui;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom/client';
 // import scss from "@scss/default.scss";
 import './assets/scss/default.scss';
 import Header from './components/Header';
+import HeaderMui from './components/HeaderMui';
 
 const App = () => (
   <div>
     <Header></Header>
+    <HeaderMui></HeaderMui>
+    <div style={{ padding: 16 }}>
+      <h1>Hello, React!</h1>
+    </div>
   </div>
 );
 
