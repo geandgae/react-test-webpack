1e38d0d8c198dd4a5ec5528a2d997bc42a63f302
g_e_out <g_e@daum.net>
2024-07-02 18:16:45 +0900

    redux 설정

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,6 +3,7 @@ import rootReducer from "./rootReducer";
 
 const store = configureStore({
   reducer: rootReducer,
+  // devTools: process.env.NODE_ENV !== 'production',
 });
 
 export default store;
